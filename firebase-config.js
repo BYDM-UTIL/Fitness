cde8bdf3afc3406814be937bea4582c9bcf4c0c4
BYDM <mor.dotan@uster.com>
2026-05-05 09:50:37 +0300

    Fix Firebase config global variable

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,8 @@
-'use strict';
-
-// Fill these values from Firebase project settings.
-// Keep this file in your private environment if you prefer.
 window.FIREBASE_CONFIG = {
-  apiKey: 'REPLACE_API_KEY',
-  authDomain: 'REPLACE_PROJECT_ID.firebaseapp.com',
-  projectId: 'REPLACE_PROJECT_ID',
-  storageBucket: 'REPLACE_PROJECT_ID.firebasestorage.app',
-  messagingSenderId: 'REPLACE_SENDER_ID',
-  appId: 'REPLACE_APP_ID'
+  apiKey: 'AIzaSyBbXc0Qq61zKaLq7OIf5akjFlpSqIDi5os',
+  authDomain: 'limor-s-fitness.firebaseapp.com',
+  projectId: 'limor-s-fitness',
+  storageBucket: 'limor-s-fitness.firebasestorage.app',
+  messagingSenderId: '80299484416',
+  appId: '1:80299484416:web:268575ff034beb48067fe5'
 };
